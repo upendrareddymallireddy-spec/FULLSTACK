30b13b988d8e65ee2997c0b3ad53263297c2c75f
upendra <upendrareddymallireddy@gmail.com>
2026-04-19 20:45:00 +0530

    Add files via upload

---
+++ TASK2/static/js/validation.js
@@ -0,0 +1,22 @@
+// -------- LOGIN VALIDATION --------
+function validateLogin() {
+    let email = document.getElementById("login_email").value;
+    let password = document.getElementById("login_password").value;
+
+    if (email === "" || password === "") {
+        alert("All fields are required!");
+        return false;
+    }
+
+    if (!email.includes("@")) {
+        alert("Enter a valid email address!");
+        return false;
+    }
+
+    if (password.length < 6) {
+        alert("Password must be at least 6 characters");
+        return false;
+    }
+
+    return true;
+}
---
+++ TASK3/db_config.py
@@ -0,0 +1,10 @@
+import mysql.connector
+
+
+def get_db_connection():
+    return mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password= "9398743824",
+        database="feedback_db"
+    )
